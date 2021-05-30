0f3c55670a18f77da9478604f51687e8f3a1c3e8
zqy-happy <zqygentle@gmail.com>
2021-05-30 13:18:54 +0800

    '123'

---
+++ python模拟点击.py
@@ -0,0 +1,15 @@
+from ctypes import *
+import pyautogui
+import time
+time.sleep(5)
+import pyautogui
+screenWidth, screenHeight = pyautogui.size()
+print(screenWidth,screenHeight)
+# while 1:
+# 	# windll.user32.SetCursorPos(600,300);
+# 	# windll.user32.SetCursorPos(900,300);
+# 	pyautogui.click(600, 670, clicks=1, interval=0.0, button='left')
+# 	time.sleep(10)
+
+
+
